c0bf8c32b6bc6db9f4850389b35da2a63e7722de
Shamira Naznin <shaminzn@gmail.com>
2020-03-08 17:45:53 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  var cities = ['New York', 'San Francisco'];
+  cities.unshift('Philadelphia');
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
